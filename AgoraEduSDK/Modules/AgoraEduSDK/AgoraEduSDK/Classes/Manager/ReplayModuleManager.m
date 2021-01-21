d2107b9a4364ab189edf9418c637668698acb9f2
SRS <sirusheng@agora.io>
2021-01-21 09:17:04 +0800

    fix replay bug

--- AgoraEduSDK/Modules/AgoraEduSDK/AgoraEduSDK/Classes/Manager/ReplayModuleManager.m
+++ AgoraEduSDK/Modules/AgoraEduSDK/AgoraEduSDK/Classes/Manager/ReplayModuleManager.m
@@ -19,53 +19,60 @@
 
 + (void)enterReplayViewControllerWithRoomId:(NSString *)roomId {
     
-    RecordInfoConfiguration *config = [RecordInfoConfiguration new];
-    config.appId = AgoraEduKeyCenter.agoraAppid;
-    config.roomUuid = roomId;
-    config.token = AgoraEduManager.shareManager.token;
-    [AppHTTPManager getRecordInfoWithConfig:config success:^(RecordModel * _Nonnull recordModel) {
+    [AgoraEduManager.shareManager.roomManager getLocalUserWithSuccess:^(EduLocalUser * _Nonnull user) {
         
-        if(recordModel.data.list.count == 0){
-            [[UIApplication sharedApplication].windows.firstObject makeToast:AgoraEduLocalizedString(@"ReplayListFailedText", nil)];
-            return;
-        }
-        
-        NSArray *resultArray = [recordModel.data.list sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-                RecordInfoModel *model1 = (RecordInfoModel*)obj1;
-                RecordInfoModel *model2 = (RecordInfoModel*)obj2;
-                NSComparisonResult result = [@(model1.startTime) compare:@(model2.startTime)];
-                return result == NSOrderedAscending;  // 降序
-        }];
-        
-        RecordInfoModel *recordInfoModel = resultArray.firstObject;
-        NSInteger status = recordInfoModel.status;
-        if(status == RecordStateRecording
-           || status == RecordStateFinished
-           || status == RecordStateWaitDownload
-           || status == RecordStateWaitConvert
-           || status == RecordStateWaitUpload) {
+        RecordInfoConfiguration *config = [RecordInfoConfiguration new];
+        config.appId = AgoraEduKeyCenter.agoraAppid;
+        config.roomUuid = roomId;
+        config.token = AgoraEduManager.shareManager.token;
+        config.userUuid = user.userUuid;
+        [AppHTTPManager getRecordInfoWithConfig:config success:^(RecordModel * _Nonnull recordModel) {
             
-            if(status != RecordStateFinished) {
-                [[UIApplication sharedApplication].windows.firstObject makeToast:AgoraEduLocalizedString(@"QuaryReplayFailedText", nil)];
+            if(recordModel.data.list.count == 0){
+                [[UIApplication sharedApplication].windows.firstObject makeToast:AgoraEduLocalizedString(@"ReplayListFailedText", nil)];
                 return;
             }
-        }
-        
-        NSString *urlString = @"";
-        if ([recordInfoModel.url containsString:@"http://"] || [recordInfoModel.url containsString:@"https://"]) {
-            urlString = recordInfoModel.url;
             
-        } else {
-            urlString = [RECORD_BASE_URL stringByAppendingString: recordInfoModel.url];
-        }
-        
-        AgoraEduReplayConfig *config = [[AgoraEduReplayConfig alloc] initWithBoardAppId:AgoraEduKeyCenter.boardAppid boardId:recordInfoModel.boardId boardToken:recordInfoModel.boardToken videoUrl:urlString beginTime:recordInfoModel.startTime endTime:recordInfoModel.endTime];
-        AgoraEduReplay *replay = [AgoraEduSDK replay:config delegate:nil];
-        if (replay != nil) {
-            [AgoraEduManager.shareManager.classroom setValue:replay forKey:@"replay"];
-        }
+            NSArray *resultArray = [recordModel.data.list sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+                    RecordInfoModel *model1 = (RecordInfoModel*)obj1;
+                    RecordInfoModel *model2 = (RecordInfoModel*)obj2;
+                    NSComparisonResult result = [@(model1.startTime) compare:@(model2.startTime)];
+                    return result == NSOrderedAscending;  // 降序
+            }];
             
-    } failure:^(NSError * _Nonnull error, NSInteger statusCode) {
+            RecordInfoModel *recordInfoModel = resultArray.firstObject;
+            NSInteger status = recordInfoModel.status;
+            if(status == RecordStateRecording
+               || status == RecordStateFinished
+               || status == RecordStateWaitDownload
+               || status == RecordStateWaitConvert
+               || status == RecordStateWaitUpload) {
+                
+                if(status != RecordStateFinished) {
+                    [[UIApplication sharedApplication].windows.firstObject makeToast:AgoraEduLocalizedString(@"QuaryReplayFailedText", nil)];
+                    return;
+                }
+            }
+            
+            NSString *urlString = @"";
+            if ([recordInfoModel.url containsString:@"http://"] || [recordInfoModel.url containsString:@"https://"]) {
+                urlString = recordInfoModel.url;
+                
+            } else {
+                urlString = [RECORD_BASE_URL stringByAppendingString: recordInfoModel.url];
+            }
+            
+            AgoraEduReplayConfig *config = [[AgoraEduReplayConfig alloc] initWithBoardAppId:AgoraEduKeyCenter.boardAppid boardId:recordInfoModel.boardId boardToken:recordInfoModel.boardToken videoUrl:urlString beginTime:recordInfoModel.startTime endTime:recordInfoModel.endTime];
+            AgoraEduReplay *replay = [AgoraEduSDK replay:config delegate:nil];
+            if (replay != nil) {
+                [AgoraEduManager.shareManager.classroom setValue:replay forKey:@"replay"];
+            }
+                
+        } failure:^(NSError * _Nonnull error, NSInteger statusCode) {
+            [[UIApplication sharedApplication].windows.firstObject makeToast:error.localizedDescription];
+        }];
+        
+    } failure:^(NSError * _Nonnull error) {
         [[UIApplication sharedApplication].windows.firstObject makeToast:error.localizedDescription];
     }];
 }
