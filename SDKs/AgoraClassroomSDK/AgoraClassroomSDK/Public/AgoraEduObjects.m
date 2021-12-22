d25cb4a5d31c8d0b2a2769cc3c15eb75f2fc965c
刘元媛 <liuyuanyuan02@agora.io>
2021-12-22 16:03:55 +0000

    Merge pull request #139 in ADUC/open-cloudclass-ios from release/apaas/2.0.0_report to release/apaas/2.0.0
    Squashed commit of the following:
    
    commit 235d912c243e77b97286cbe1f512854b3d576ee8
    Author: cavansu <suruoxi@agora.io>
    Date:   Wed Dec 22 23:49:30 2021 +0800
    
        [ENH]:update code format
    
    commit 505342e117ca4c82e85aec466ca1d2fa915298e3
    Merge: 973120d4 784baf50
    Author: cavansu <suruoxi@agora.io>
    Date:   Wed Dec 22 22:52:51 2021 +0800
    
        [ENH]:merged, LoginViewController
    
    commit 973120d43ebf0d8e1f4f9241b32cc69576c5a9c7
    Merge: 9b41435e 200790a1
    Author: cavansu <suruoxi@agora.io>
    Date:   Wed Dec 22 16:13:05 2021 +0800
    
        Merge branch 'release/apaas/2.0.0' of ssh://git.agoralab.co/aduc/open-cloudclass-ios into release/apaas/2.0.0
    
    commit 9b41435e575240155d05a2ce8cb48000807b2774
    Merge: 70b98ca8 058e93f3
    Author: cavansu <suruoxi@agora.io>
    Date:   Wed Dec 22 13:03:42 2021 +0800
    
        Merge branch 'release/apaas/2.0.0' of ssh://git.agoralab.co/aduc/open-cloudclass-ios into release/apaas/2.0.0
    
    commit 70b98ca8d8a9b7f6aa140a1e55e6905ea345e1c8
    Merge: 9f34e99a 89842cdd
    Author: cavansu <suruoxi@agora.io>
    Date:   Wed Dec 22 13:00:35 2021 +0800
    
        [ENH]:merge, LoginViewController
    
    commit 9f34e99ae643d670c5fc1788039b27753dacb08e
    Merge: c8bcba69 e9dc9d59
    Author: cavansu <suruoxi@agora.io>
    Date:   Tue Dec 21 14:14:25 2021 +0800
    
        Merge branch 'release/apaas/2.0.0' of ssh://git.agoralab.co/aduc/open-cloudclass-ios into release/apaas/2.0.0
    
    commit c8bcba6958931a0f2077af8a7a8f01645071d084
    Merge: 27c8bdc5 e3c65431
    Author: cavansu <suruoxi@agora.io>
    Date:   Tue Dec 21 11:46:21 2021 +0800
    
        Merge branch 'release/apaas/2.0.0' of ssh://git.agoralab.co/aduc/open-cloudclass-ios into release/apaas/2.0.0
    
    commit 27c8bdc517194bdd500105a120b4ade107d43ef4
    Merge: c0e9e293 9e8955c1
    Author: cavansu <suruoxi@agora.io>
    Date:   Mon Dec 20 16:53:27 2021 +0800
    
        Merge branch 'release/apaas/2.0.0' of ssh://git.agoralab.co/aduc/open-cloudclass-ios into release/apaas/2.0.0
    
    commit c0e9e29319ca4c95b216ed8d67872752e4903578
    Author: cavansu <suruoxi@agora.io>
    Date:   Mon Dec 20 16:45:08 2021 +0800
    
        [ENH]:update pod
    
    commit 671d4f30b5e93181520727efbe5919867c5f7aed
    Author: cavansu <suruoxi@agora.io>
    Date:   Mon Dec 20 16:29:58 2021 +0800
    
        [ENH]:update build sh

--- SDKs/AgoraClassroomSDK/AgoraClassroomSDK/Public/AgoraEduObjects.m
+++ SDKs/AgoraClassroomSDK/AgoraClassroomSDK/Public/AgoraEduObjects.m
@@ -124,38 +124,33 @@
     // Register widgets
     NSMutableDictionary<NSString *, AgoraWidgetConfig *> *widgets = [NSMutableDictionary dictionary];
     // TODO: replace rtm to im (chat)
-    AgoraWidgetConfig *chat = [[AgoraWidgetConfig alloc]
-                               initWithClass:[ChatWidget class]
-                               widgetId:@"easemobIM"];
+    AgoraWidgetConfig *chat = [[AgoraWidgetConfig alloc] initWithClass:[ChatWidget class]
+                                                              widgetId:@"easemobIM"];
     widgets[chat.widgetId] = chat;
     
     // AgoraSpreadRenderWidget
-    AgoraWidgetConfig *spreadRender = [[AgoraWidgetConfig alloc]
-                                       initWithClass:[AgoraSpreadRenderWidget class]
-                                       widgetId:@"big-window"];
+    AgoraWidgetConfig *spreadRender = [[AgoraWidgetConfig alloc] initWithClass:[AgoraSpreadRenderWidget class]
+                                                                      widgetId:@"big-window"];
     widgets[spreadRender.widgetId] = spreadRender;
     
     // AgoraCloudWidget
-    AgoraWidgetConfig *cloudWidgetConfig = [[AgoraWidgetConfig alloc]
-                                            initWithClass:[AgoraCloudWidget class]
-                                            widgetId:@"AgoraCloudWidget"];
+    AgoraWidgetConfig *cloudWidgetConfig = [[AgoraWidgetConfig alloc] initWithClass:[AgoraCloudWidget class]
+                                                                           widgetId:@"AgoraCloudWidget"];
     widgets[cloudWidgetConfig.widgetId] = cloudWidgetConfig;
     
     // AgoraWhiteboardWidget
-    AgoraWidgetConfig *whiteboardConfig = [[AgoraWidgetConfig alloc]
-                                           initWithClass:[AgoraWhiteboardWidget class]
-                                           widgetId:@"netlessBoard"];
+    AgoraWidgetConfig *whiteboardConfig = [[AgoraWidgetConfig alloc] initWithClass:[AgoraWhiteboardWidget class]
+                                                                          widgetId:@"netlessBoard"];
     // RTM IM Widget
-    AgoraWidgetConfig *rtm = [[AgoraWidgetConfig alloc]
-                               initWithClass:[AgoraRtmIMWidget class]
-                               widgetId:@"AgoraChatWidget"];
+    AgoraWidgetConfig *rtm = [[AgoraWidgetConfig alloc] initWithClass:[AgoraRtmIMWidget class]
+                                                             widgetId:@"AgoraChatWidget"];
     widgets[rtm.widgetId] = rtm;
     
     NSString *courseFolder = [NSString stringWithFormat:@"%@/%@",NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0],@"AgoraDownload"];
     whiteboardConfig.extraInfo = @{@"coursewareDirectory":courseFolder,
                                    @"useMultiViews": @YES,
                                    @"autoFit": @NO};
-
+    
     widgets[whiteboardConfig.widgetId] = whiteboardConfig;
     return widgets;
 }
@@ -181,7 +176,6 @@
     exts[answerExt.appIdentifier] = answerExt;
     exts[answerExt.appIdentifier] = voteExt;
     return exts;
-    
 }
 @end
 
