6fa9bfc52904609ee3bff9112da9c6f8061ee759
Lyy <liuyuanyuan02@agora.io>
2022-08-17 18:37:51 +0800

    [ENH]:add deinit stop timer

--- SDKs/AgoraEduUI/Classes/Views/Render/FcrWindowRenderViews.swift
+++ SDKs/AgoraEduUI/Classes/Views/Render/FcrWindowRenderViews.swift
@@ -215,6 +215,10 @@ class FcrWindowRenderView: UIView {
         waveView.stopAnimating()
     }
     
+    deinit {
+        stopHighlightTimer()
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
