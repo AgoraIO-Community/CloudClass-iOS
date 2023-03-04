f383c4138455986b01dc49efae4031bb24892570
cavansu <suruoxi@agora.io>
2023-03-04 17:05:07 +0800

    [ENH]:publish & delete tag sh

--- Products/Scripts/Publish/delete_published_tag.sh
+++ Products/Scripts/Publish/delete_published_tag.sh
@@ -1,8 +1,24 @@
+# difference
+Repo_Name="open-cloudclass-ios"
+
+# cd this file path
+cd $(dirname $0)
+echo pwd: `pwd`
+
+# import 
+. ../../../../apaas-cicd-ios/Products/Scripts/Other/v1/operation_print.sh
+
+# pramater
 SDK_Name=$1
 SDK_Version=$2
 
-Tag=${SDK_Name}_v${SDK_Version}
+parameterCheckPrint ${SDK_Name}
+parameterCheckPrint ${SDK_Version}
 
-git tag -d ${Tag}
-git push origin :refs/tags/${Tag}
-git push originGithub :refs/tags/${Tag}
+# path
+CICD_Root_Path=../../../../apaas-cicd-ios
+CICD_Products_Path=${CICD_Root_Path}/Products
+CICD_Scripts_Path=${CICD_Products_Path}/Scripts
+
+# publish
+${CICD_Scripts_Path}/SDK/Publish/v1/delete_published_tag.sh ${SDK_Name} ${SDK_Version} ${Repo_Name}
