cb462fb241a2236ab33a6e2cd49b62a03f36a3d7
cavansu <suruoxi@agora.io>
2023-03-14 20:39:32 +0800

    [ENH]:updated sh

--- ci/build/build_ios.sh
+++ ci/build/build_ios.sh
@@ -87,12 +87,14 @@ echo build_time: $build_time
 echo release_version: $release_version
 echo short_version: $short_version
 echo BUILD_NUMBER: ${BUILD_NUMBER}
+echo Branch_Name: ${open_cloudclass_ios_branch}
 
 export all_proxy=http://10.80.1.174:1080
 
 # difference
 Repo_Name="open-cloudclass-ios"
 SDK_Array=(AgoraEduUI AgoraClassroomSDK_iOS)
+Branch_Name=${open_cloudclass_ios_branch}
 
 # import
 . ../apaas-cicd-ios/Products/Scripts/Other/v1/operation_print.sh
@@ -115,11 +117,11 @@ do
   
   # publish
   if [ "${Package_Publish}" = true ]; then
-    ${CICD_Pack_Path}/v1/package.sh ${SDK} ${Repo_Name} ${BUILD_NUMBER}
+    ${CICD_Pack_Path}/v1/package.sh ${SDK} ${Repo_Name}
 
     errorPrint $? "${SDK} package"
       
-    ${CICD_Upload_Path}/v1/upload_artifactory.sh ${SDK} ${Repo_Name} ${is_official_build}
+    ${CICD_Upload_Path}/v1/upload_artifactory.sh ${SDK} ${Branch_Name} ${Repo_Name} ${is_official_build}
 
     errorPrint $? "${SDK} upload"
   fi
